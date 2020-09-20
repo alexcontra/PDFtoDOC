3d62564fb19f67793cfefe83bb1e8255352778e5
Alex Contra <contraalex8@gmail.com>
2020-09-20 17:59:51 +0300

    improved generator

--- pdftoDOC/src/main/java/generateDOC/generate.java
+++ pdftoDOC/src/main/java/generateDOC/generate.java
@@ -4,12 +4,10 @@ import com.aspose.pdf.Document;
 public class generate {
     public generate(String fileName)
     {
-        Document doc = new Document(fileName);
+        Document doc = new Document("pdftoDOC/"+fileName);
         DocSaveOptions saveOptions = new DocSaveOptions();
-        saveOptions.setMode(DocSaveOptions.RecognitionMode.Flow);
-        saveOptions.setRelativeHorizontalProximity(2.5f);
-        saveOptions.setRecognizeBullets(true);
-        String newName = fileName.replace(".pdf","");
+        saveOptions.setFormat(DocSaveOptions.DocFormat.DocX);
+        String newname = fileName.replace(".pdf","");
         doc.save(fileName+"new.docx",saveOptions);
     }
 }
